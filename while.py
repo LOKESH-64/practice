984e66e48910eb731d294915c158647b6088bccf
TATA LOKESH <lokesh64lt@gmail.com>
2026-05-08 23:13:22 +0530

    while loop practice

---
+++ while.py
@@ -0,0 +1,97 @@
+# print 1 to 10
+# n=1
+# while n<=10:
+#     print(n) 
+#     n+=1
+
+#print 10 to 1
+# n= 10
+# while n>=1:
+#     print(n)
+#     n-=1 
+
+# Print all even numbers from 1 to 20
+# n= 2 
+# while n<=20:
+#     print(n,end=' ')
+#     n+=2
+
+# Print all odd numbers from 1 to 20
+# n= 1 
+# while n<=20:
+#     print(n,end=' ')
+#     n+=2
+
+
+# Print numbers from 1 to N (take N as input)
+# x= int(input("enter a number: "))
+# n = 1
+# while n<=x:
+#     print(n)
+#     n+=1 
+
+#Find the sum of first N numbers
+# n = int(input("enter a number:"))
+# i=1
+# total=0
+# while i<=n:
+#     total+=i 
+#     i+=1
+# print("sum",total)
+
+#Find the factorial of a number
+# n = int(input("enter a number:"))
+# i =1
+# fact = 1
+# while i<=n:
+#     fact*=i
+#     i+=1
+# print(fact)
+
+#Count how many numbers are divisible by 3 between 1 and N
+
+# n = int(input("enter a number:"))
+# i = 1 
+# count = 0
+# while i<=n:
+#     if i%3==0:
+#         count+=1
+#     i+=1
+# print(count)
+
+#Find the sum of even numbers from 1 to N
+# n = int(input("enter a number :"))
+# i = 2 
+# total = 0
+# while i <=n:
+#     total+=i
+#     i+=2
+# print(total)
+
+
+#Print the multiplication table of a number
+
+# n = int(input("enter a number :"))
+# i =1 
+# while i<=10:
+#     print(f"{n}x{i}={n*i}") 
+#     i+=1
+
+
+#Count the number of digits in a number
+
+# n= int(input("number :"))
+# count =0
+# while n>0:
+#     count+=1
+#     n//=10
+# print(count)
+
+
+#12. Reverse number
+n = int(input("enter a number :"))
+rev = 0
+while n>0:
+    digit = n%10
+    rev = re
+
