16bf309454bc0f860e8a358dc6ab9417bc9a68d7
TATA LOKESH <lokesh64lt@gmail.com>
2026-05-16 23:02:30 +0530

    p

---
+++ list.py
@@ -0,0 +1,6 @@
+# these are used to store the multiple single varible
+family_list=["daddy","mummy","me","me","mummy","daddy"]
+
+print(family_list)
+family_list[3:]=["shop","money"]
+print(family_list)
