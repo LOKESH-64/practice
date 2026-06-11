9b86086925b41cde539ef49784c9c0c6c6f7e1cb
TATA LOKESH <lokesh64lt@gmail.com>
2026-06-11 22:44:12 +0530

    ,

---
+++ inserting a element.py
@@ -0,0 +1,10 @@
+lst=["books","encyclopedia","stationary","papers"]
+"""lst.insert(4,"scale")
+print(lst)
+tropical=["mango","orange","papaya"]
+lst.extend(tropical)"""
+
+lst.remove("encyclopedia")
+print(lst)
+lst.pop(1)
+print(lst)
