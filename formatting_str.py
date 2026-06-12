f5ecb78f6c52bb65c00daa52dabaf2807c830ef0
TATA LOKESH <lokesh64lt@gmail.com>
2026-06-12 21:39:58 +0530

    .

---
+++ formatting_str.py
@@ -0,0 +1,8 @@
+price=65
+context=f"the product is fiexd price {price} rupees "
+print(context)# here we have placed a placeholder {  } and passed the argument 
+1
+context2=f"the product is  always {price:1}"
+print(context2)
+cnt=f"item is {10*10} rupees"
+print(cnt)
