46c44aae58c1663d90fecbd5cea8b1379360a22c
TATA LOKESH <lokesh64lt@gmail.com>
2026-06-13 21:45:06 +0530

    '

---
+++ tupels.py
@@ -0,0 +1,6 @@
+tuple=("hi","hello","hi","elsee","hi") #tuple is unchangable
+#these are included in round braces
+print(type(tuple))
+#we can pass all types of values in the tuples
+tuple1=("hell",43,True,"hevan")
+print(tuple1)
