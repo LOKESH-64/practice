dfc8cef398d1621250fb68018785636bae8996ca
TATA LOKESH <lokesh64lt@gmail.com>
2026-06-19 20:14:03 +0530

    :

---
+++ functions18_02.py
@@ -0,0 +1,48 @@
+'''properties :
+1) Code reusability.
+2) Reduce in code duplication.
+3) Reduce in the lines of logic.
+4) Reduce in the memory location.  
+
+
+Types of functions:
+1) pre-defined function :     print(), int(),len() these are some pre defined functions which are inbuilt. 
+2) user-defined functions :   created by the user 
+
+2.1) functions with def keyword :
+2.2) lambda function | anonymus functions 
+2.3) recursive functions
+
+--- based on arguments ----
+1) empty functions or zero argumented functions 
+2) argumented functions 
+3) 
+
+
+'''
+
+
+# #2.1) functions with def keyword :
+# # syntax :
+# def function_name():# function decelartion 
+#     block of code
+#     block of code
+#     block of code
+
+
+# function_name()  # function calling 
+
+def add():
+    a,b = 10 ,20 
+    c = a+b
+    print(c)
+add()
+    
+
+for i in range(5):
+    add()
+
+
+
+
+        
