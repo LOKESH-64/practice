b992fd75b94d3da56a9b9c6f87bbd546c9d4fb74
TATA LOKESH <lokesh64lt@gmail.com>
2026-06-20 22:08:10 +0530

    %

---
+++ functions.types_19_02.py
@@ -0,0 +1,51 @@
+# def fn_1():
+#     a = 100
+#     b = 200
+#     c = a+b
+#     print("this is function")
+#     return a,b,c/a,b*a,'hello',f"addition of a is {c}"
+
+# print(fn_1())
+# print(a)
+# print(a)
+
+# b =fn_1() # function object 
+
+
+# argumented functions 
+# m = 2000
+# n = 1000
+# def fn_1(a,b):
+#     x=a 
+#     c=x+b
+#     return a,b,x,c
+
+# s = 11
+# t = 22
+# b=fn_1(6,7)
+# print(b)
+# print(fn_1(4,5))
+# print(fn_1(s,t))
+# print(fn_1(int(input()),int(input())))
+# print(fn_1(a=45,b=46))
+
+# x,y,z = 100 ,200,300
+# a,b,c = [10,20,30]
+# print(a)
+# print(b)
+# print(c)
+# print(x)          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
