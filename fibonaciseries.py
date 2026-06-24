d9a9f833fa9d6aedaeee52daa7b92659c6e2232b
TATA LOKESH <lokesh64lt@gmail.com>
2026-06-24 21:38:13 +0530

    !

---
+++ fibonaciseries.py
@@ -0,0 +1,14 @@
+#0,1,1,2,3,5,8,13,21,34
+
+# need = int(input("enter the req no of fibonacii series:"))
+# a = 1
+# b = 0
+# print(b,end=',')
+# print(a,end=',')
+# need-= 2
+# for i in range(need):
+#     c = a+b
+#     print(c,end=',')
+#     b=a
+#     a=c 
+
