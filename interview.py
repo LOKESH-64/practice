e96d194715e080e6f6b0635dedc0d4c09015bbe8
TATA LOKESH <lokesh64lt@gmail.com>
2026-06-30 21:47:43 +0530

    #

---
+++ interview.py
@@ -0,0 +1,60 @@
+# a = input()
+# need = a[3]
+
+# x=0 
+# count = 0
+# while x<len(a):
+#     if a[x] == need:
+#         count+=1
+
+#         if count == a.count(a[3]):
+#             print(f"the last occurance of {a[3] } is at{x}")
+#             break
+
+
+# replace a vowel characters with *
+
+# x = "adsfds"
+# b = ''
+
+# a = 0
+# while a<len(x):
+#     y=x[a]
+#     if x[a] in ['a','e','i','o','u','A','E','I','O','U']:
+#         y ='*'
+#         b+=y
+#     else:
+#         b+=y
+#     a+=1
+# x=b
+
+# print('x =',x)
+
+
+
+# a = [34,3,76,734,63,56]
+# for i in range(len(a)):
+#     if a[i]%7==0:
+#         a[i] = "hi"
+# print('a',a)    
+
+# for i in range(5):
+#     x = 1
+#     for j in range(5):
+#         print(x,end='*')
+#         x+=1
+#     print()
+
+
+# for i in range(5):
+#     x = 1 
+#     a = ''
+#     for j in range(5):
+
+#         a+=str(x)+'*'
+#         x+=1
+#     print(a.rstrip("*"),end = ' ')
+#     print()
+
+
+
